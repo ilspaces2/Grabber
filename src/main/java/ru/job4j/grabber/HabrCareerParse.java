dcd9afb48e216653f117c15f6063847f2b714fa4
I_M <moskva.88@mail.ru>
2022-04-04 15:54:46 +0300

    2.4. SqlRuParse [#285213 #284035]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,8 +5,13 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.HarbCareerDateTimeParser;
+import ru.job4j.model.Post;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -30,33 +35,27 @@ import java.io.IOException;
  * linkElement.attr("href");
  */
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
-    public static void main(String[] args) throws IOException {
-        HabrCareerParse habrCareerParse = new HabrCareerParse();
-        for (int numPage = 1; numPage <= 5; numPage++) {
-            System.out.printf(">>> PAGE %d <<<%n", numPage);
-            Connection connection = Jsoup.connect(PAGE_LINK + "?page=" + numPage);
-            Document document = connection.get();
-            Elements rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
-                Element titleElement = row.select(".vacancy-card__title").first();
-                Element linkElement = titleElement.child(0);
-                String vacancyName = titleElement.text();
-                Element dateElement = row.select(".vacancy-card__date").first();
-                Element timeElement = dateElement.child(0);
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s%n", vacancyName, link, timeElement.attr("datetime"));
-                System.out.printf("%s%n-----%n", habrCareerParse.retrieveDescription(link));
-            });
-        }
+    private final DateTimeParser dateTimeParser;
+
+    private int numPage = 1;
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
     }
 
-    private String retrieveDescription(String link) {
+    public static void main(String[] args) {
+        HabrCareerParse habrCareerParse = new HabrCareerParse(new HarbCareerDateTimeParser());
+        habrCareerParse.setNumPage(5);
+        habrCareerParse.list(PAGE_LINK).forEach(System.out::println);
+    }
+
+    private static String retrieveDescription(String link) {
         Connection connection = Jsoup.connect(link);
         String text = null;
         try {
@@ -68,5 +67,45 @@ public class HabrCareerParse {
             e.printStackTrace();
         }
         return text;
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        if (numPage < 1) {
+            throw new IllegalArgumentException("NumPage less 1");
+        }
+        List<Post> posts = new ArrayList<>();
+        try {
+            for (int count = 1; count <= numPage; count++) {
+                Connection connection = Jsoup.connect(link + "?page=" + count);
+                Document document = connection.get();
+                Elements rows = document.select(".vacancy-card__inner");
+                rows.forEach(row -> {
+                    Element titleElement = row.select(".vacancy-card__title").first();
+                    Element linkElement = titleElement.child(0);
+                    String vacancyName = titleElement.text();
+                    Element dateElement = row.select(".vacancy-card__date").first();
+                    Element timeElement = dateElement.child(0);
+                    String linkPost = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                    posts.add(new Post(
+                            vacancyName,
+                            linkPost,
+                            retrieveDescription(linkPost),
+                            dateTimeParser.parse(timeElement.attr("datetime"))
+                    ));
+                });
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return List.copyOf(posts);
+    }
+
+    public int getNumPage() {
+        return numPage;
+    }
+
+    public void setNumPage(int numPage) {
+        this.numPage = numPage;
     }
 }
